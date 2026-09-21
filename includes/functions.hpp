0c885097df29653a929d261c2d6b7377688939bb
Nithila Kalaimani <nithilak@illinois.edu>
2026-09-20 22:54:42 -0500

    finished UI

--- includes/functions.hpp
+++ includes/functions.hpp
@@ -7,31 +7,51 @@
 
 #include "constants.hpp"
 
+//I don't use const iterators but maybe they could be implemented
+//also perhaps there should be less copy and pasting in main
+
+//not my code, prints a csv file
 void PrintFile(std::string filename);
 
+//not my code, // Function to split a CSV line by commas
 std::vector<std::string> parseCSVLine(const std::string& line);
 
+//prints out all expenses for each month the year
+void PrintAllExpenses(const Year& year);
+
+//prints out all expenses for the month
 double PrintExpenses(const std::set<Expense>& expenses);
 void PrintExpenses(const std::set<Expense>& expenses, const Month& month);
 void PrintExpenses(const std::set<Expense>& expenses, std::chrono::month curr_month, std::chrono::year year);
 void PrintExpenses(const Month& month);
 void PrintExpenses(const Month& month, std::chrono::year year);
-void PrintExpenses(Year& year, std::chrono::month curr_month);
+void PrintExpenses(const Year& year, std::chrono::month curr_month);
 
-
+//reads in a csv file under the folder in includes/YEARExpenses/MONTHYEAR and uses it to fill month.expenses
+//also updates each month and year total
 double PopulateExpenses(const std::string& filename, Month& month);
 double PopulateExpenses(const std::string& filename, Year& year, std::chrono::month curr_month);
 double PopulateExpenses(Year& year);
 
 void PrintAnnualTotal(const Year& year);
 
+//inserts a year with all 12 month files formatted like includes/YEARExpenses/MONTHYEAR into years
 Year& InsertYear(int year);
 
+//adds an expense to a month
 int AddExpense(Year& year, std::chrono::month curr_month);
 
+//deletes an expense from a month
 int DeleteExpense(Year& year, std::chrono::month curr_month);
 int DeleteExpense(std::chrono::year year, Month& month);
 
-int UpdateMonthFile(Year& year, std::chrono::month curr_month);
+//updates the month's csv file to match its data
+int UpdateMonthFile(const Year& year, std::chrono::month curr_month);
+int UpdateTotalsFile(const Year& year);
+
+int PromptInsertYear();
+Year& PromptYear();
+std::chrono::month PromptMonth();
+int PromptYearErase();
 
 #endif // FUNCTIONS_HPP
--- src/driver.cc
+++ src/driver.cc
@@ -15,38 +15,152 @@ int main() {
   // PrintFile("includes/january.csv");
   // PrintFile("includes/total_expenses.csv");
 
-  Year& year_2026 = InsertYear(2026);
+  // Year& year_2026 = InsertYear(2026);
 
-  // Month& January2026 = year_2026.months[static_cast<unsigned int>(curr_month) - 1];
+  // // Month& January2026 = year_2026.months[static_cast<unsigned int>(curr_month) - 1];
 
-  std::cout << std::endl;
-
-  for (int i = 1; i < 13; i++) {
-    std::chrono::month curr_month = static_cast<std::chrono::month>(i);
-    PrintExpenses(year_2026, curr_month);
-  }
+  // std::cout << std::endl;
 
   // for (int i = 1; i < 13; i++) {
   //   std::chrono::month curr_month = static_cast<std::chrono::month>(i);
-  //   PopulateExpenses("includes/" + year_str + "Expenses/" + std::format("{:%B}", curr_month) + year_str + ".csv", year_2026, curr_month);
   //   PrintExpenses(year_2026, curr_month);
   // }
-  // Month& curr_month = year.months[static_cast<unsigned int>(month) - 1];
-  // std::vector<Expense>& expenses = curr_month.expenses;
 
-  PrintAnnualTotal(year_2026);
+  // PrintAnnualTotal(year_2026);
 
-  std::chrono::month month_to_add = std::chrono::February;
-  if (AddExpense(year_2026, month_to_add)) {
-    UpdateMonthFile(year_2026, month_to_add);
+  // std::chrono::month month_to_add = std::chrono::February;
+  // if (AddExpense(year_2026, month_to_add)) {
+  //   UpdateMonthFile(year_2026, month_to_add);
+  // }
+  // PrintExpenses(year_2026, month_to_add);
+
+  // if (DeleteExpense(year_2026, month_to_add)) {
+  //   UpdateMonthFile(year_2026, month_to_add);
+  // }
+
+  // PrintExpenses(year_2026, month_to_add);
+
+  std::string line;
+
+  std::cout << "Enter q to quit." << std::endl;
+  
+  if (!PromptInsertYear()) {
+    return 0;
   }
-  PrintExpenses(year_2026, month_to_add);
 
-  if (DeleteExpense(year_2026, month_to_add)) {
-    UpdateMonthFile(year_2026, month_to_add);
+  std::cout << std::endl;
+
+  while (true) {
+    std::cout << "Insert year: 0, Choose year: 1, See available years: 2, Delete year: 3" << std::endl;
+    std::cout << "Print all expenses: 4, Print month expenses: 5" << std::endl;
+    std::cout << "Add expense: 6, Delete expense: 7" << std::endl; 
+
+    unsigned num;
+
+    while (std::getline(std::cin, line)) {
+      if (line == "q") {
+        return 0;
+      }
+
+      std::stringstream ss(line);
+
+      if (ss >> num) {
+        if (num == 0) {
+          PromptInsertYear();
+          std::cout << std::endl;
+        } else if (num == 1) {
+
+          Year& year = PromptYear();
+          std::cout << std::endl;
+          // std::chrono::year curr_year = year.year;
+
+          std::cout << "Year: " << year.year << std::endl;
+          std::cout << "Return to all years: 0" << std::endl;
+          std::cout << "Print all expenses: 4, Print month expenses: 5" << std::endl;
+          std::cout << "Add expense: 6, Delete expense: 7" << std::endl; 
+
+          while (std::getline(std::cin, line)) {
+            if (line == "q") {
+              return 0;
+            }
+
+            std::stringstream ss(line);
+
+            if (ss >> num) {
+              if (num == 0) {
+                std::cout << std::endl;
+                break;
+              }
+              if (num == 4) {
+                PrintAllExpenses(year);
+              } else {
+                std::chrono::month month_to_add = PromptMonth();
+
+                if (num == 5) {
+                  PrintExpenses(year, month_to_add);
+                } else if (num == 6) {
+                  if (AddExpense(year, month_to_add)) {
+                    UpdateMonthFile(year, month_to_add);
+                    // UpdateTotalsFile(year);
+                  }
+                  std::cout << std::endl; //temp
+                } else if (num == 7) {
+                  if (DeleteExpense(year, month_to_add)) {
+                    UpdateMonthFile(year, month_to_add);
+                    // UpdateTotalsFile(year);
+                  }
+                  std::cout << std::endl; //temp
+                }
+              }
+            } else {
+              std::cout << "Invalid input.\n";
+            }
+            std::cout << "Year: " << year.year << std::endl;
+            std::cout << "Return to all years: 0" << std::endl;
+            std::cout << "Print all expenses: 4, Print month expenses: 5" << std::endl;
+            std::cout << "Add expense: 6, Delete expense: 7" << std::endl;
+          }
+        } else if (num == 2) {
+          std::cout << std::endl;
+          std::cout << "Available years: ";
+          for (const auto& [key, value] : years) {
+            std::cout << key << " ";
+          }
+          std::cout << "\n" << std::endl;
+        } else if (num == 3) {
+          PromptYearErase();
+        } else {
+          Year& year = PromptYear();
+
+          if (num == 4) {
+            std::cout << std::endl;
+            PrintAllExpenses(year);
+          } else {
+            std::chrono::month month_to_add = PromptMonth();
+
+            if (num == 5) {
+              PrintExpenses(year, month_to_add);
+            } else if (num == 6) {
+              if (AddExpense(year, month_to_add)) {
+                UpdateMonthFile(year, month_to_add);
+              }
+              std::cout << std::endl; //temp
+            } else if (num == 7) {
+              if (DeleteExpense(year, month_to_add)) {
+                UpdateMonthFile(year, month_to_add);
+              }
+              std::cout << std::endl; //temp
+            }
+          }
+        }
+        break;
+      } else {
+        std::cout << "Invalid input.\n";
+      }
+    }
   }
 
-  PrintExpenses(year_2026, month_to_add);
+
 
   return 0;
 }
